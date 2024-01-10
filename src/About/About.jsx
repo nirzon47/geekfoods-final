88c1e781f9471885cd35fa3d4add56c87fb3ae22
Nirzon Karmakar <nirzon47@gmail.com>
2024-01-10 23:43:26 +0530

    minor UI optimizations

--- src/About/About.jsx
+++ src/About/About.jsx
@@ -37,28 +37,28 @@ const About = () => {
 							<input
 								type='text'
 								placeholder='Your Name'
-								className='text-body-color border-[f0f0f0] focus:border-primary w-full rounded border py-3 px-[14px] text-base outline-none '
+								className='w-full px-4 py-3 text-sm text-base border rounded outline-none border-zinc-300'
 							/>
 						</div>
 						<div className='mb-6'>
 							<input
 								type='email'
 								placeholder='Your Email'
-								className='text-body-color border-[f0f0f0] focus:border-primary w-full rounded border py-3 px-[14px] text-base outline-none '
+								className='w-full px-4 py-3 text-sm text-base border rounded outline-none border-zinc-300'
 							/>
 						</div>
 						<div className='mb-6'>
 							<input
 								type='text'
 								placeholder='Your Phone'
-								className='text-body-color border-[f0f0f0] focus:border-primary w-full rounded border py-3 px-[14px] text-base outline-none '
+								className='w-full px-4 py-3 text-sm text-base border rounded outline-none border-zinc-300'
 							/>
 						</div>
 						<div className='mb-6'>
 							<textarea
 								rows='6'
 								placeholder='Your Message'
-								className='text-body-color border-[f0f0f0] focus:border-primary w-full resize-none rounded border py-3 px-[14px] text-base outline-none '
+								className='w-full px-4 py-3 text-sm text-base border rounded outline-none resize-none border-zinc-300'
 							></textarea>
 						</div>
 						<div>
